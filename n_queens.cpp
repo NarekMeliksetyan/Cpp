5f1aeb9d5e2adb5ef997db66d39b973f541e8df7
Narek <mnk80898@gmail.com>
2019-07-30 16:02:06 +0300

    I did smth

--- n_queens.cpp
+++ n_queens.cpp
@@ -17,51 +17,50 @@
 
 using namespace std;
  
-
 bool IsSafePos(int*, int, int, int);
 void backtracking(int, int&, int, int*);
 
 int main() {
-    int n, count = 0;
-    cout << "Enter n: ";
-    cin >> n;
+	int n, count = 0;
+	cout << "Enter n: ";
+	cin >> n;
 
-    int *board = new int[n];
-    for (int i = 0; i < n; i++)
-        board[i] = 0;
+	int *board = new int[n];
+	for (int i = 0; i < n; i++)
+		board[i] = 0;
 
-    backtracking(0, count, n, board);
-    
-    if (count == 0)
-	    cout << "There is no solution.";
-    else if (count == 1)
-	    cout << "There is 1 solution.";
-    else
-	    cout << "There are " << count << " solutions.";
-    cout << endl;
+	backtracking(0, count, n, board);
+	
+	if (count == 0)
+		cout << "There is no solution.";
+	else if (count == 1)
+		cout << "There is 1 solution.";
+	else
+		cout << "There are " << count << " solutions.";
+	cout << endl;
 
-    delete []board;
-    return 0;
+	delete []board;
+	return 0;
 }
 
 bool IsSafePos(int *board, int k, int y, int N) {
-    int i = 0;
-    while (i < k && y != board[i] && abs(k - i) != abs(y - board[i]))
-	    i++;
-    if (i == k)
-        return true;
-    else
-        return false;
+	int i = 0;
+	while (i < k && y != board[i] && abs(k - i) != abs(y - board[i]))
+		i++;
+	if (i == k)
+		return true;
+	else
+		return false;
 }
  
 void backtracking(int k, int &count, int N, int *board) {
-    int i, y;
-    for (y = 0; y < N; y++) {
-        if (IsSafePos(board, k, y, N)) {
-            board[k] = y;
-            if (k == N - 1)
-                count++;
-            backtracking(k + 1, count, N, board);
-        }
-    }
+	int i, y;
+	for (y = 0; y < N; y++) {
+		if (IsSafePos(board, k, y, N)) {
+			board[k] = y;
+			if (k == N - 1)
+				count++;
+			backtracking(k + 1, count, N, board);
+		}
+	}
 }
