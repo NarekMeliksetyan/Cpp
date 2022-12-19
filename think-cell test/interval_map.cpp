22da9dd169fa1301ac5e47cf4c7e6bed9a296cb8
nkhaldi <mnk80898@gmail.com>
2022-12-19 23:24:14 +0400

    formated code via vscode

--- think-cell test/interval_map.cpp
+++ think-cell test/interval_map.cpp
@@ -1,14 +1,19 @@
 #include <map>
-template<typename K, typename V>
-class interval_map {
+
+template <typename K, typename V>
+
+class interval_map
+{
 	friend void IntervalMapTest();
 	V m_valBegin;
-	std::map<K,V> m_map;
+	std::map<K, V> m_map;
+
 public:
 	// constructor associates whole range of K with val
-	interval_map(V const& val)
-	: m_valBegin(val)
-	{}
+	interval_map(V const &val)
+		: m_valBegin(val)
+	{
+	}
 
 	// Assign value val to interval [keyBegin, keyEnd).
 	// Overwrite previous values in this interval.
@@ -16,16 +21,21 @@ public:
 	// includes keyBegin, but excludes keyEnd.
 	// If !( keyBegin < keyEnd ), this designates an empty interval,
 	// and assign must do nothing.
-	void assign( K const& keyBegin, K const& keyEnd, V const& val ) {
-// INSERT YOUR SOLUTION HERE
+	void assign(K const &keyBegin, K const &keyEnd, V const &val)
+	{
+		// INSERT YOUR SOLUTION HERE
 	}
 
 	// look-up of the value associated with key
-	V const& operator[]( K const& key ) const {
-		auto it=m_map.upper_bound(key);
-		if(it==m_map.begin()) {
+	V const &operator[](K const &key) const
+	{
+		auto it = m_map.upper_bound(key);
+		if (it == m_map.begin())
+		{
 			return m_valBegin;
-		} else {
+		}
+		else
+		{
 			return (--it)->second;
 		}
 	}
