282d5982f8c5d93637ba1cae020f9d8fd4fd6772
nkhaldi <mnk80898@gmail.com>
2023-03-26 22:06:20 +0300

    added argc and argv

--- pointer_slide.c
+++ pointer_slide.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char **argv)
 {
     unsigned steps = 100;
-
-    int start = 100;
+    int start = 1;
     int *ptr = &start;
 
     while (steps--)
     {
         printf("%p: ", ptr);
         if (*ptr)
-        {
             printf("%d\n", *ptr);
-        }
         else
-        {
             printf("empty\n");
-        }
         ptr++;
     }
     
